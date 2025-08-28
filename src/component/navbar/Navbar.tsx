bb8f0564ab98ea71ea9f3fa5de79723e04412cef
anjali1102 <ac.chauhan1102@gmail.com>
2025-08-28 20:57:34 +0530

    [Feat]: Added toggle theme

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -1,13 +1,24 @@
-// import { Moon } from "lucide-react";
-import { Sun } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "retro");
+
+  useEffect(() => {
+    const html = document.documentElement;
+    html.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  function toggleTheme() {
+    setTheme(theme === "dark" ? "retro" : "dark");
+  }
   return (
     <div>
       <div className="navbar bg-base-100 shadow-sm flex justify-between">
         <a className="btn btn-ghost text-xl pointer">Rick and Morty</a>
-        <button className="pointer">
-          <Sun />
+        <button className="cursor-pointer" onClick={toggleTheme}>
+          {theme === "dark" ? <Sun /> : <Moon />}
         </button>
       </div>
     </div>
