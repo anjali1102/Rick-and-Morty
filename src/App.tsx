5eca88d6aac34ee20a09eeddf8c265f319493d98
anjali1102 <ac.chauhan1102@gmail.com>
2025-08-29 01:32:51 +0530

    [Feat]: added code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
-import CharacterDetail from "./pages/CharacterDetail";
+
+const Home = React.lazy(() => import("./pages/Home"));
+const CharacterDetail = React.lazy(() => import("./pages/CharacterDetail"));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/items/:id" element={<CharacterDetail />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen w-full text-xl text-gray-500">
+            <span className="loading loading-dots loading-xl"></span>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/items/:id" element={<CharacterDetail />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
